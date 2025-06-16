3c785d06c866b71e85ad02fa5aeb5ea056f3536f
Stevanus Febrian <stevanus.febrian@binus.edu>
2025-06-16 15:47:12 +0700

    disable import tensorflow

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from tensorflow.keras.models import load_model #pip install tensorflow
+#from tensorflow.keras.models import load_model
 import pickle
 
 # Set the layout
